97a3249eda6df785a266f90904ec77028db3b7cf
Keith Stouffer <keith.stouffer@bluemedora.com>
2016-11-03 12:49:55 -0400

    1-4 done

--- src/i_introduction/_4_Lambdas/Lambdas.kt
+++ src/i_introduction/_4_Lambdas/Lambdas.kt
@@ -1,5 +1,7 @@
 package i_introduction._4_Lambdas
 
+import com.google.common.base.Predicate
+import com.google.common.collect.Iterables
 import util.TODO
 import util.doc4
 
@@ -21,7 +23,12 @@ fun todoTask4(collection: Collection<Int>): Nothing = TODO(
     documentation = doc4(),
     references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+fun task4(collection: Collection<Int>): Boolean {
+
+
+    return collection.any { it % 42 == 0}
+
+}
 
 
 
